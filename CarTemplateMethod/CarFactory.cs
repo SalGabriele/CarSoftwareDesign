53e9c7bd807aa9486beba6651e69b8e04511d86b
Gabrrr <gabriele.saletyte@gmail.com>
2017-10-09 12:18:57 +0300

    added variables to abstarct class

--- CarTemplateMethod/CarFactory.cs
+++ CarTemplateMethod/CarFactory.cs
@@ -8,8 +8,29 @@ namespace CarTemplateMethod
 {
     public abstract class CarFactory
     {   
-        private double shippingTime;
-        private double constructingTime;
+        protected Random rnd = new Random();
+
+        //Region based variables
+        protected Dictionary<string, double> distance;
+        
+        protected double packingTime;
+        protected double shippingTime;
+        protected double unpackingTime;
+        protected double allShippingTime;
+
+        protected int packingTimeDeflection;
+        protected int shippingTimeDeflection;
+        protected int shippingSpeed; //km/h
+
+        //Gear Box Type based variables
+        protected double getReadyTime;
+        protected double constructingTime;
+        protected double finishingTime;
+        protected double allConstructingTime;
+
+        protected int getReadyTimeDeflection;
+        protected int constructingTimeDeflection;
+        protected int finishingTimeDeflection;
         
         protected abstract double GetShippingTime();
         protected abstract double GetConstructingTime();
