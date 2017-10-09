b5b63bc6c2f72c174e9cd317651be34be02883af
Gabrrr <gabriele.saletyte@gmail.com>
2017-10-09 12:41:49 +0300

    mech from Europe

--- CarTemplateMethod/MechanicFromEurope.cs
+++ CarTemplateMethod/MechanicFromEurope.cs
@@ -8,14 +8,45 @@ namespace CarTemplateMethod
 {
     class MechanicFromEurope: CarFactory
     {
-        public override void BuyDetails()
+       protected override double GetShippingTime()
        {
-           //buy from Europe
+            distance = new Dictionary<string, double>()
+            {
+                {"France", 2267}, //km
+                {"Germany", 1324},
+                {"UK", 2649}
+            };
+            packingTime = 2.5;
+            unpackingTime = 1.5;
+            packingTimeDeflection = 10;
+            shippingTimeDeflection = 15;
+            shippingSpeed = 100;
+
+            packingTime = DoDeflection(packingTime, packingTimeDeflection);
+            var country = distance.ToList()[rnd.Next(distance.Count)];
+            Console.WriteLine("Gear Box details is shipping from " + country.Key + " and it will take " + shippingTime + "hours");
+            shippingTime = DoDeflection(country.Value/shippingSpeed, shippingTimeDeflection);
+            unpackingTime = DoDeflection(unpackingTime, packingTimeDeflection);
+            allShippingTime = packingTime + shippingTime + unpackingTime;
+
+            return allShippingTime;
        }
 
-        public override void MakeGearBox()
+        protected override double GetConstructingTime()
        {
-           //make mechanic gear box
+            getReadyTime = 3;
+            constructingTime = 25;
+            finishingTime = 7;
+            getReadyTimeDeflection = 6;
+            constructingTimeDeflection = 22;
+            finishingTimeDeflection = 10;
+
+            getReadyTime = DoDeflection(getReadyTime, getReadyTimeDeflection);
+            constructingTime = DoDeflection(constructingTime, constructingTimeDeflection);
+            finishingTime = DoDeflection(finishingTime, finishingTimeDeflection);
+            allConstructingTime = getReadyTime + constructingTime + finishingTime;
+
+            return allConstructingTime;
        }
     }
 }
